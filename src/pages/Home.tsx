31e112d01bc8c22b264dd5f541dc89c9502f97a1
Ryoichi <sgtsit118@gmail.com>
2026-04-05 09:43:43 +0900

    App.tsxの実装、HomeとRecordListのページの実装（ガワだけ） #18

---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+// Issue-009: 仮実装（Home画面の実コンポーネントはIssue-013で実装）
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
---
+++ src/pages/RecordList.tsx
@@ -0,0 +1,6 @@
+// Issue-009: 仮実装（RecordList画面の実コンポーネントはIssue-014で実装）
+const RecordList = () => {
+  return <div>RecordList</div>;
+};
+
+export default RecordList;
